19e477d7e855125438fd1e1d0946722a3f801ab4
Mark Hording <markhording@gmail.com>
2025-06-16 12:00:49 +0700

    manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
